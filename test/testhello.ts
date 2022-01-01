8db4dc17e78b5a057d99ee9ef44509252c44de55
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-01-02 06:45:00 +0700

    Testfile #1
    create testfile using ts based, setup before deploy and dont' forget to inser the name of the contract to make integration with the Ethereum blockchai by using ABI

---
+++ test/testhello.ts
@@ -0,0 +1,14 @@
+import _ from "@nomiclabs/hardhat-waffle";
+
+import { ethers } from "hardhat";
+import { expect } from "chai";
+
+describe("Hello test", () => {
+  it("should be get the hello world string", async function(){
+    //1. Setup, just call the name of your contract (contract name), not the file name.
+    const OurContract = await ethers.getContractFactory("HelloContract");
+
+    //2. Deploy our contract using deploy and deployed function from nomiclabs/hardhat-ethers
+    //3. Call our functions to test    
+  })
+})
